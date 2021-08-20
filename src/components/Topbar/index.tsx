330c53aee18e90d37fdfc2fbca4e94855f884259
ruijadom <ruijadomingues@gmail.com>
2021-08-20 19:52:17 +0100

    feat(tailwind): install and setup tailwind

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,4 +1,4 @@
-import '../../tailwind.css';
+import '../../css/tailwind.css';
 
 import React from 'react';
 
@@ -7,7 +7,7 @@ import { ITopbar } from './types';
 const Topbar = ({ children }: ITopbar) => {
   return (
     <>
-      <div className="flex items-center h-10 bg-gray-900 text-white px-4">
+      <div className="flex items-center h-10 px-4 text-white bg-gray-900">
         {children}
       </div>
     </>
